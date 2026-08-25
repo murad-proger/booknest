5eeaa1564b365d3415e3d7cc0930a531928ccf56
murad-proger <muradiusifov@gmail.com>
2026-08-25 12:00:10 +0400

    fix: show admin link based on session"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,21 @@
+"use client"
+
 import styles from "./Header.module.css"
 
 import Link from "next/link";
 import HeaderCart from "../Cart/HeaderCart/HeaderCart";
 import HeaderAuth from "./HeaderAuth/HeaderAuth";
+import { useSession } from "next-auth/react";
 
-export default async function Header() {
+export default function Header() {
+  const session = useSession()
 
   return (
     <header className={styles.header}>
       <Link href={"/"}>Booknest</Link>
       <nav className={styles.nav}>
-        <Link href={"/admin"}>Admin</Link>
         <Link href={"/books"}>Books</Link>
+        {session?.data?.user.role === "ADMIN" && (<Link href={"/admin"}>Admin</Link>)}
       </nav>
       <div className={styles.leftPanel}>
         <HeaderCart />
