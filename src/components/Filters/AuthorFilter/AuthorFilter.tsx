fe7e0e348d0284f57adfc9abe48644b5146f5e9e
murad-proger <muradiusifov@gmail.com>
2026-09-09 15:49:46 +0400

    refactor(filters): migrate Sort/Author to Radix primitives, restyle PriceFilter

--- src/components/Filters/AuthorFilter/AuthorFilter.tsx
+++ src/components/Filters/AuthorFilter/AuthorFilter.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { useSearchParams, useRouter } from "next/navigation"
+import * as Checkbox from "@radix-ui/react-checkbox"
 
-import styles from "./AuthorFilter.module.css";
+import styles from "./AuthorFilter.module.css"
 
 export default function AuthorFilter({ authors }: { authors: string[] }) {
   const searchParams = useSearchParams()
@@ -10,45 +11,54 @@ export default function AuthorFilter({ authors }: { authors: string[] }) {
 
   const selectedAuthors = searchParams.getAll("authors")
 
-  const onAuthorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onAuthorChange = (author: string, checked: boolean) => {
     const params = new URLSearchParams(searchParams)
 
-    const { checked, value } = e.target
-
-    params.delete("authors");
+    params.delete("authors")
 
     let updatedAuthors = [...selectedAuthors]
 
-    if(checked) {
-      updatedAuthors.push(value)
+    if (checked) {
+      updatedAuthors.push(author)
     } else {
-      updatedAuthors = updatedAuthors.filter(
-        author => author !== value
-      )
+      updatedAuthors = updatedAuthors.filter((a) => a !== author)
     }
 
-    updatedAuthors.forEach(author => {
-      params.append("authors", author)
+    updatedAuthors.forEach((a) => {
+      params.append("authors", a)
     })
-    
-    router.replace(`/books?${params.toString()}`)  
+
+    router.replace(`/books?${params.toString()}`)
   }
-  
+
   return (
     <fieldset className={styles.filterAuthors}>
       <legend>Author(s):</legend>
 
       {authors.map((author) => (
         <label key={author} className={styles.authorItem}>
-          <input
-            type="checkbox"
+          <Checkbox.Root
+            className={styles.checkbox}
             name="authors"
             value={author}
-            onChange={onAuthorChange}
-          />
+            defaultChecked={selectedAuthors.includes(author)}
+            onCheckedChange={(checked) => onAuthorChange(author, checked === true)}
+          >
+            <Checkbox.Indicator className={styles.indicator}>
+              <CheckIcon />
+            </Checkbox.Indicator>
+          </Checkbox.Root>
           <span>{author}</span>
         </label>
       ))}
     </fieldset>
-  );
+  )
+}
+
+function CheckIcon() {
+  return (
+    <svg width="10" height="10" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M2 6L5 9L10 3" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
 }
--- src/components/Filters/SortFilter/SortFilter.tsx
+++ src/components/Filters/SortFilter/SortFilter.tsx
@@ -3,40 +3,81 @@
 import styles from "./SortFilter.module.css"
 
 import { useSearchParams, useRouter } from "next/navigation"
+import * as Select from "@radix-ui/react-select"
 
-export default function SortFilter () {
+const SORT_OPTIONS = [
+  { value: "title-asc", label: "Title A-Z" },
+  { value: "title-desc", label: "Title Z-A" },
+  { value: "price-asc", label: "Price \u2191" },
+  { value: "price-desc", label: "Price \u2193" },
+]
+
+export default function SortFilter() {
   const searchParams = useSearchParams()
-  const route = useRouter()
+  const router = useRouter()
 
-  const defaultSort = searchParams.get('sort') ?? ''
+  const defaultSort = searchParams.get("sort") ?? ""
 
-  const onSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+  const onSortChange = (value: string) => {
     const params = new URLSearchParams(searchParams)
-    
-    const { value } = e.target
 
-    if(value) {
+    if (value) {
       params.set("sort", value)
     } else {
       params.delete("sort")
     }
-    
-    route.replace(`/books?${params.toString()}`)
+
+    router.replace(`/books?${params.toString()}`)
   }
 
   return (
     <label className={styles.filtersSelect}>
       <span>Sort by:</span>
-      <select
-        name="sort"
-        defaultValue={defaultSort}
-        onChange={onSortChange}
-      >
-        <option value="title-asc">Title A-Z</option>
-        <option value="title-desc">Title Z-A</option>
-        <option value="price-asc">Price &#8593;</option>
-        <option value="price-desc">Price &#x2193;</option>
-      </select>
+      <Select.Root name="sort" defaultValue={defaultSort} onValueChange={onSortChange}>
+        <Select.Trigger className={styles.trigger} aria-label="Sort by">
+          <Select.Value placeholder="Default" />
+          <Select.Icon className={styles.icon}>
+            <ChevronIcon />
+          </Select.Icon>
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content
+            className={styles.content}
+            position="popper"
+            sideOffset={4}
+            side="bottom"
+            avoidCollisions={false}
+          >
+            <Select.Viewport>
+              {SORT_OPTIONS.map((option) => (
+                <Select.Item key={option.value} value={option.value} className={styles.item}>
+                  <Select.ItemText>{option.label}</Select.ItemText>
+                  <Select.ItemIndicator className={styles.indicator}>
+                    <CheckIcon />
+                  </Select.ItemIndicator>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
     </label>
+  )
+}
+
+function ChevronIcon() {
+  return (
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M2.5 4.5L6 8L9.5 4.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+}
+
+function CheckIcon() {
+  return (
+    <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M2 6L5 9L10 3" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
   )
 }
